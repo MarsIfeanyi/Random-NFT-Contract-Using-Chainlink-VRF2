fb657f1bc10db521bc935941add8beebacb3aafe
MarsIfeanyi <marschalice@gmail.com>
2023-09-14 00:59:22 +0100

    Initial Commit

---
+++ scripts/writeContracts/removeLiquidityETH.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+import {
+  UNISWAPv2_CONTRACT_ADDRESS,
+  USDC_UNI_HOLDER,
+  ADDRESS_TO,
+  UNI_ADDRESS,
+  USDC_ADDRESS,
+  USDC_HOLDER,
+} from "../contractAddress/address";
+
+async function main() {}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
